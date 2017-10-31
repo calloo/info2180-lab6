a8fae17b994c42371ddeb112e883bb4a05c1895c
Michael Calloo <michael.calloo@mymona.uwi.edu>
2017-10-31 07:17:13 +0000

    Excercise 3

--- app.js
+++ app.js
@@ -11,3 +11,17 @@ function notify(){
     xhttp.open("GET", "request.php?q=definition", true);
     xhttp.send();
 }
+
+function lookup(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.status == 200 && this.readyState == 4){
+            document.getElementById("result").innerHTML= this.responseText;
+        }
+    }
+    
+    xhttp.open("GET", "request.php?q=" + document.forms["browse"]["q"].value, true);
+    xhttp.send();
+    
+    return false;
+}
